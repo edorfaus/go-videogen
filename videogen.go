6afd50edf2cf1ee5c53bad5390bd954521a8c365
Frode Austvik <edorfaus@xepher.net>
2022-08-02 02:05:15 +0000

    Add encoding to webm (yuva420p) via ffmpeg pipe

--- videogen.go
+++ videogen.go
@@ -6,13 +6,17 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"io"
 	"os"
+	"os/exec"
 
 	"github.com/edorfaus/go-videogen/anim"
 	"github.com/edorfaus/go-videogen/frameloop"
 )
 
 var Width, Height, Rate int
+
+var RawOutput bool
 
 func main() {
 	if err := run(); err != nil {
@@ -25,6 +29,7 @@ func args() {
 	flag.IntVar(&Width, "w", 8, "width of video")
 	flag.IntVar(&Height, "h", 8, "height of video")
 	flag.IntVar(&Rate, "r", 10, "frame rate of video")
+	flag.BoolVar(&RawOutput, "raw", false, "write raw RGBA output")
 	flag.Parse()
 }
 
@@ -40,11 +45,19 @@ func run() error {
 
 	frames := make(chan *frameloop.Frame, 1)
 
-	fl := frameloop.New(ctx, os.Stdout, frames, Rate)
+	// Set up output video stream encoding
+	rawVideo, ffmpeg, err := encoder(ctx, os.Stdout)
+	if err != nil {
+		return err
+	}
+
+	// Start the frame output loop
+	fl := frameloop.New(ctx, rawVideo, frames, Rate)
 	defer fl.Stop()
 
 	frames <- frame
 
+	// Run the animation
 	c := color.NRGBA{0, 0, 0, 255}
 	for i := 0; i < Rate*10; i++ {
 		select {
@@ -57,8 +70,72 @@ func run() error {
 		}
 	}
 
+	// Stop the frame loop, wait for it to be done
 	fl.Stop()
 	<-fl.Done()
+	if err := fl.Err(); err != nil {
+		return err
+	}
 
-	return fl.Err()
+	// Shut down the encoder, if necessary
+	if ffmpeg != nil {
+		if err := rawVideo.Close(); err != nil {
+			return err
+		}
+		if err := ffmpeg.Wait(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func encoder(
+	ctx context.Context, target io.WriteCloser,
+) (io.WriteCloser, *exec.Cmd, error) {
+	if RawOutput {
+		return target, nil, nil
+	}
+
+	cmd := exec.CommandContext(
+		ctx, "ffmpeg",
+		"-hide_banner", "-nostdin",
+
+		// input args
+		"-an", // no audio
+		"-f", "rawvideo", "-pixel_format", "rgba",
+		"-framerate", fmt.Sprintf("%v", Rate),
+		"-video_size", fmt.Sprintf("%vx%v", Width, Height),
+		// attempt to avoid buffering latency
+		"-avioflags", "direct",
+		"-fflags", "nobuffer",
+		"-probesize", "32",
+		"-analyzeduration", "0",
+		"-fpsprobesize", "0",
+		// input file: stdin
+		"-i", "-",
+
+		// output args
+		"-f", "webm", "-pix_fmt", "yuva420p",
+		// attempt to avoid buffering latency
+		"-avioflags", "direct",
+		"-fflags", "flush_packets",
+		"-flush_packets", "1",
+		// output file: stdout
+		"-",
+	)
+
+	cmd.Stdout = target
+	cmd.Stderr = os.Stderr
+
+	input, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return nil, nil, err
+	}
+
+	return input, cmd, nil
 }
